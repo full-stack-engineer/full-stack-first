d1d7c81621c5e67b7205bcb49fa8abbb885024e4
ebijun1007 <ebijun1007@gmail.com>
2019-09-14 15:56:43 +0900

    generate controllers & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'omniauth_callbacks' ,
+    sessions: 'users/sessions'
+  }
   root 'user#index'
 end
 

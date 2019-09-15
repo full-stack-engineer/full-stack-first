f6c91dfc5c9160f8dffafd748410e055a8468cc2
Kenta Aikawa <kalupas226@gmail.com>
2019-09-15 11:18:01 +0900

    add dependent to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :parent_tasks
+  has_many :parent_tasks, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

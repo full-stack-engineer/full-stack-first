1a582a094978b1339d952a98de9dd1159e845c67
Kenta Aikawa <kalupas226@gmail.com>
2019-09-15 17:54:49 +0900

    rootをtasksに

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'omniauth_callbacks',
     sessions: 'users/sessions'
   }
-  root 'user#index'
+  root 'tasks#index'
   resources :child_tasks, only:[:edit, :new]
   resources :parent_tasks, only:[:edit, :new]
 end

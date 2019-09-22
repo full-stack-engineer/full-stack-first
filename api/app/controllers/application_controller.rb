79473d981d8d95d6c7f5d7ce8bceac758f68b236
Kenta Aikawa <kalupas226@gmail.com>
2019-09-22 23:23:50 +0900

    modify api

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
         before_action :skip_session
-        before_action :configure_permitted_parameters
+        before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
     def skip_session

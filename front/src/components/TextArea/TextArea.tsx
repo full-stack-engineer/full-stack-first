e723a6a84b96f574d3ba53807145ae778760a18e
three4c <three4c@gmail.com>
2019-09-23 20:45:55 +0900

    useStateでコードを短く

--- front/src/components/TextArea/TextArea.tsx
+++ front/src/components/TextArea/TextArea.tsx
@@ -1,43 +1,23 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./TextArea.scss";
 
 interface TextAreaInterface {
-    textHeight: number;
-    state: string;
+    placeholder: string;
 }
 
-export default class TextArea extends Component<{}, TextAreaInterface> {
-    textAreaRef: React.RefObject<HTMLTextAreaElement>;
-    constructor(props: TextAreaInterface) {
-        super(props);
-        this.textAreaRef = React.createRef();
+const TextArea: React.FC<TextAreaInterface> = props => {
+    const [textHeight, setTextHeight] = useState(1.6)
 
-        this.state = {
-            textHeight: 1.6,
-            state: ""
-        };
-    }
-
-    private getHieghtOfTextArea = (): void => {
-        if (this.textAreaRef.current !== null) {
-            this.setState({
-                textHeight: this.textAreaRef.current.value.split("\n").length * 2.5
-            })
-        }
-    }
-
-    render() {
-        console.log(this.state.textHeight);
-        return (
-            <div className="TextArea">
-                <textarea
-                    className="TextArea__input"
-                    onChange={this.getHieghtOfTextArea}
-                    style={{ height: `${this.state.textHeight}rem` }}
-                    ref={this.textAreaRef}
-                    placeholder="タスクを追加してみよう！"
-                />
-            </div>
-        )
-    }
+    return (
+        <div className="TextArea">
+            <textarea
+                className="TextArea__input"
+                onChange={(e) => { setTextHeight(e.target.value.split("\n").length * (1.6 + .3)) }}
+                style={{ height: `${textHeight}rem` }}
+                placeholder={props.placeholder}
+            />
+        </div>
+    )
 }
+
+export default TextArea;

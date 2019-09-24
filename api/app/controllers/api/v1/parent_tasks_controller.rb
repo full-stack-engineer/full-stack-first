7a976577c7459e0406ef803b395c0fa0368b03d4
Kenta Aikawa <kalupas226@gmail.com>
2019-09-24 21:19:19 +0900

    Modify api (#25)
    * task_params読み取れてないっぽいけど一旦プッシュ
    
    * parent_task一覧取得時にuser_idを入力しなくても良いようにした
    また、ログインしていてもタスクの所持者でなければupdateとdestroyができないようにした
    
    * modify キレイキレイ

--- api/app/controllers/api/v1/parent_tasks_controller.rb
+++ api/app/controllers/api/v1/parent_tasks_controller.rb
@@ -2,15 +2,16 @@ module Api
     module V1
         class ParentTasksController < ApplicationController
             before_action :set_parent_task
-            before_action :authenticate_api_user! 
+            before_action :authenticate_api_user!
 
             def index
-                parent_tasks = ParentTask.where(user_id: params[:user_id]).order(created_at: :asc)
+                parent_tasks = ParentTask.where(user_id: current_api_user.id).order(created_at: :asc)
                 render json: { status: 'SUCCESS', message: 'Loaded parent_tasks', data: parent_tasks }
             end
 
             def create
                 parent_task = ParentTask.new(task_params)
+                parent_task.user_id = current_api_user.id
                 if parent_task.save
                     render json: { status: 'SUCCESS', data: parent_task}
                 else
@@ -19,15 +20,17 @@ module Api
             end
 
             def destroy
+                return render json: { status: 'ERROR', message: 'You do not have permission' } unless current_api_user.id == @parent_task.user_id
                 @parent_task.destroy
                 render json: { status: 'SUCCESS', message: 'Deleted the task', data: @parent_task }
             end
 
             def update
+                return render json: { status: 'ERROR', message: 'You do not have permission' } unless current_api_user.id == @parent_task.user_id
                 if @parent_task.update(task_params)
                     render json: { status: 'SUCCESS', message: 'Updated the task', data: @parent_task }
                 else
-                    render json: { status: 'ERROR', message: 'Not updated', data: @parent_task }
+                    render json: { status: 'ERROR', message: 'Not updated', data: @parent_task.errors }
                 end
             end
 
@@ -37,7 +40,7 @@ module Api
                 end
 
                 def task_params
-                    params.require(:parent_task).permit(:content, :progress, :user_id)
+                    params.permit(:content, :progress)
                 end
 
         end

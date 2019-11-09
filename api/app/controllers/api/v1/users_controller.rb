0f528706d172ba962c0cac5d48350c1687f1cc54
Kenta Aikawa <kalupas226@gmail.com>
2019-11-09 23:34:37 +0900

    modify register api

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -3,13 +3,18 @@ module Api
     class UsersController < ApplicationController
         def create
           @user = User.create! user_params
-      
+
           payload = Jwt::TokenProvider.refresh_tokens @user
           payload[:user] = @user
-      
-          render json: { status: :success, data: payload }
+
+          render json: { status: :success,
+                         token: { access_token: payload[:access_token],
+                                  refresh_token: payload[:refresh_token]
+                                },
+                         user: payload[:user]
+                       }
         end
-      
+
         private
         def user_params
           params.require(:user)
